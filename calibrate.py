df54d44f6671704cb732c1382c9b248257931a7f
David Kirkby <dkirkby@uci.edu>
2019-08-23 17:48:29 -0700

    Update calibrate

--- calibrate.py
+++ calibrate.py
@@ -5,15 +5,12 @@ import numpy as np
 from stxldriver.camera import Camera
 
 
-def calibrate_dark(
-    camera, temperature=10.,
-    exptimes=[0., 1., 2., 4., 8., 16., 32., 64.], ncycles=5, min_history=10):
-    """Perform a sequence of dark calibration exposures.
-    """
-    ttotal = ncycles + np.sum(exptimes) + min_history
-    print('Estimated time: {0:.1f}s'.format(ttotal))
-    # Cool the camera down.
-    camera.write_setup(Bin=1, CCDTemperatureSetpoint=temperature, CoolerState=1)
+def initialize(camera, binning, temperature, min_history=10):
+    print('Rebooting...')
+    camera.reboot()
+    time.sleep(30)
+    camera.write_setup(Bin=binning, CCDTemperatureSetpoint=temperature, CoolerState=1, Fan=2, FanSetpoint=50)
+    time.sleep(15)
     print('Waiting for cooldown to {0:.1f}C...'.format(temperature))
     history = []
     while True:
@@ -23,37 +20,55 @@ def calibrate_dark(
         print('  T={0:.3f}, Tavg={1:.3f}'.format(history[-1], tavg))
         if len(history) >= min_history and np.abs(tavg - temperature) < 0.05:
             break
-    # Loop over cycles.
-    for cycle in range(ncycles):
-        print('Starting cycle {0} / {1}...'.format(cycle + 1, ncycles))
-        # Loop over exposure times.
-        for exptime in exptimes:
-            print('   Starting {0:.0f}s exposure...'.format(exptime))
-            camera.start_exposure(ExposureTime=exptime, ImageType=0)
-            now = time.time()
-            end = now + exptime + 5
-            while now < end:
-                time.sleep(1.)
-                Tnow = float(camera.call_api('ImagerGetSettings.cgi?CCDTemperature'))
-                if np.abs(Tnow - temperature) > 0.1:
-                    print('  * temperature is not stable (now {0:.3f}C).'.format(Tnow))
-                state = camera.call_api('CurrentCCDState.cgi')
-                if state == '3':
-                    break
-            if state != '3':
-                print('  *** Found unexpected CCD state after exposure: {0}.'.format(state))
-            else:
-                fname = 'data/calib_{0:.1f}_{1:.1f}_{2}.fits'.format(temperature, exptime, cycle)
-                print('   Writing {0}...'.format(fname))
-            camera.save_exposure(fname)
+
+
+def take_exposure(camera, exptime, fname, temperature, timeout=10):
+    camera.start_exposure(ExposureTime=exptime, ImageType=0, Contrast=1)
+    # Monitor the temperature and cooler power during the exposure.
+    cutoff = time.time() + exptime + timeout
+    state = '?'
+    temp_history, pwr_history = [], []
+    while time.time() < cutoff:
+        # Read the current state, but keep going in case of a network problem.
+        try:
+            temp_now = float(camera.call_api('ImagerGetSettings.cgi?CCDTemperature'))
+            pwr_now = float(camera.call_api('ImagerGetSettings.cgi?CoolerPower'))
+            state = camera.call_api('CurrentCCDState.cgi')
+            temp_history.append(temp_now)
+            pwr_history.append(pwr_now)
+            # State: 0=Idle, 2=Exposing
+            if state == '0':
+                break
+        except RuntimeError as e:
+            logging.warning(e)
+        time.sleep(1.0)
+    msg = ('T {0:4.1f}/{1:4.1f}/{2:4.1f}C PWR {3:2.0f}/{4:2.0f}/{5:2.0f}%'
+            .format(*np.percentile(temp_history, (0, 50, 100)),
+                    *np.percentile(pwr_history, (0, 50, 100))))
+    print(msg)
+    if state != '0':
+        logging.warning('Found unexpected CCD state {0} for {1}.'.format(state, fname))
+    else:
+        if np.all(np.array(pwr_history) == 100) and np.min(temp_history) > temperature + 2:
+            print('Detected cooling latchup!')
+        # Read the data from the camera, always using the same filename.
+        camera.save_exposure(fname)
+        print('Saved {0}'.format(fname))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='STXL calibration.')
+    parser.add_argument('--url', default='http://10.0.1.3',
+        help='Camera interface URL to use')
+    parser.add_argument('-b', '--binning', type=int, choices=(1, 2, 3), default=1,
+        help='Camera pixel binning to use')
     parser.add_argument('-T', '--temperature', type=float, default=15.,
         help='Temperature setpoint to use in C')
-    parser.add_argument('-n', '--ncycles', type=int, default=5,
-        help='Number of calibration cycles to perform')
+    parser.add_argument('--nzero', type=int, default=10,
+        help='Number of zero-length exposures to take')
     args = parser.parse_args()
-    C = Camera()
-    calibrate_dark(C, temperature=args.temperature, ncycles=args.ncycles)
+
+    C = Camera(URL=args.url, verbose=False)
+    initialize(C, args.binning, args.temperature)
+    for i in range(args.nzero):
+        take_exposure(camera, 0., 'data/zero_{0:03d}.fits'.format(i), args.temperature)
