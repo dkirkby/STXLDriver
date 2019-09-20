81f1c55d3e0ec4199e9af1b6dca92e47170cf216
David Kirkby <dkirkby@uci.edu>
2019-09-20 16:39:14 -0700

    Start work on 0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='stxldriver',
-    version='0.1',
+    version='0.2.dev0',
     description='HTTP Driver for SBIG STXL Cameras',
     url='http://github.com/dkirkby/STXLDriver',
     author='David Kirkby',
     author_email='dkirkby@uci.edu',
     license='MIT',
-    packages=['stxldriver'],
+    packages=['stxldriver', 'stxldriver.scripts'],
     install_requires=['numpy', 'requests'],
     entry_points = {
         'console_scripts': [
